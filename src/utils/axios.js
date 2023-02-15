ce9f28c3ef6eba4675a8c0ecfbaa2d823ac80235
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2023-02-15 14:16:51 -0800

    Removing request timeout

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create( {
-    baseURL: import.meta.env.VITE_BACKEND_ENDPOINT,
-    timeout: 1000
+    baseURL: import.meta.env.VITE_BACKEND_ENDPOINT
 } )
