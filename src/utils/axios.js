75ad7ebd92d955106213e47ac6f88b9d1aa1fd0e
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2023-02-17 13:54:16 -0800

    Fixing axios config

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create( {
-    baseURL: import.meta.env.VITE_BACKEND_ENDPOINT
+    baseURL: `${import.meta.env.VITE_BACKEND_ENDPOINT}/api/v1`
 } )
